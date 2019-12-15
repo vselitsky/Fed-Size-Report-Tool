34710809d4fdca1843de3de44247f0d9769e1b25
olya fomicheva <olyafomicheva@olyas-MacBook-Pro.local>
2019-12-15 15:12:52 -0500

    IP Address change

--- report-tool/static/main.js
+++ report-tool/static/main.js
@@ -14,25 +14,7 @@
 
 	$(function(){
 
-    var x='<select name="field">{% for col in columns %}<option value="{{col}}">{{col}}</option>{% endfor %}</select>'
-
-    $('#more_fields').click(function() {
-
-         $(x).insertBefore('#more_fields');
-
-    });
-
-    
-    $("#test").change(function(){
-        var selected_option = $(this).children("option:selected").val();
-        var y='<select name="f">{% for index, row in fed_sizes.iterrows() %}<option value="{{row}}">{{row}}</option>{% endfor %}</select>'
-
-        $(y).insertBefore('#more_fields');
-
-
-    });
-
-
+   
     
 
    });
